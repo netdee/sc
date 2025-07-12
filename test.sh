74ae2b6c55689e4dac1fd987b9c9d41096de2065
netdee <147313060+netdee@users.noreply.github.com>
2025-07-12 13:34:35 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,19 @@
+#!/bin/bash
+#kiritossh
 fun_online() {
     _ons=$(ps -x | grep sshd | grep -v root | grep priv | wc -l)
-    [[ -e /etc/openvpn/openvpn-status.log ]] && _onop=$(grep -c "10.8.0" /etc/o>
-    [[ -e /etc/default/dropbear ]] && _drp=$(ps aux | grep dropbear | grep -v g>
+    [[ -e /etc/openvpn/openvpn-status.log ]] && _onop=$(grep -c "10.8.0" /etc/openvpn/openvpn-status.log) || _onop="0"
+    [[ -e /etc/default/dropbear ]] && _drp=$(ps aux | grep dropbear | grep -v grep | wc -l) _ondrp=$(($_drp - 1)) || _ondrp="0"
     _onli=$(($_ons + $_onop + $_ondrp))
     _onlin=$(printf '%-5s' "$_onli")
-    _quantity=200
     CURRENT_ONLINES="$(echo -e "${_onlin}" | sed -e 's/[[:space:]]*$//')"
-    echo "[ {\"onlines\"😕"$CURRENT_ONLINES\",\"limite\"😕"${_quantity}\"} ]" >>
-<} ]" > /var/www/html/server/online_app.json
+    echo "{\"onlines\":\"$CURRENT_ONLINES\",\"limite\":\"2500\"}" > /var/www/html/server/online_app
+    echo "{\"onlines\":\"$CURRENT_ONLINES\",\"limite\":\"2500\"}" > /var/www/html/server/online_app.json
     echo $CURRENT_ONLINES  > /var/www/html/server/online
     echo $CURRENT_ONLINES  > /var/www/html/server/online.json
 }
 while true; do
-    echo 'กำลังตรวจสอบ...'
-        fun_online > /dev/null 2>&1
-        sleep 15s
+    echo 'verificando...'
+	fun_online > /dev/null 2>&1
+	sleep 15s
 done
